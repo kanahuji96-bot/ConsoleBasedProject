becbd8a595e0161cd4cc1786f807903c2bc1f848
MuhammadRaflyRamadani <muhamadraflyramadani@smkwikrama.sch.id>
2025-09-11 17:03:12 +0700

    Add project files.

---
+++ ConsoleBasedProject/data_umur.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBasedProject
+{
+    internal class data_umur
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukkan nama Anda: ");
+            string nama = Console.ReadLine();
+
+            Console.WriteLine("Masukkan nama Anda: ");
+            int umur = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Halo " + nama + ", umur anda " + umur + " tahun");
+            Console.ReadLine();
+        }
+    }
+}
